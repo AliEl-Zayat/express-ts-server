b9360e7deaca93d294fa8df407328f45616ccf3c
AliEl-Zayat <110483901+AliEl-Zayat@users.noreply.github.com>
2024-03-05 14:40:02 +0200

    Adds Functionality of generating metrics

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,25 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = process.env.PORT || 8080;
 
+const generateData = (selectedMonth: string) => {
+	const daysInMonth = moment(selectedMonth).daysInMonth();
+	const data = [];
+	for (let i = 0; i < daysInMonth; i++) {
+		const date = moment(selectedMonth).date(i + 1);
+		const label = date.format("D MMM");
+		const value = Math.floor(Math.random() * 1000); // Generating random value
+		data.push({ id: i, label, value });
+	}
+	return data;
+};
+
 app.get("/", (_req: Request, res: Response) => {
   return res.send("Express Typescript on Vercel");
+});
+
+app.get("/metrics", (req: Request, res: Response) => {
+	res.setHeader("Content-Type", "application/json");
+	res.status(200).json(generateData(req.body?.month as string));
 });
 
 app.get("/ping", (_req: Request, res: Response) => {
